1ab54540691ad609ec2ee70b96a0723cbf47699d
David Schlachter <david+gh@schlachter.ca>
2019-02-01 13:08:59 -0500

    Update Sparkle

--- itunes-concatenator/Sparkle.framework/Versions/A/Headers/SUAppcast.h
+++ itunes-concatenator/Sparkle.framework/Versions/A/Headers/SUAppcast.h
@@ -9,22 +9,27 @@
 #ifndef SUAPPCAST_H
 #define SUAPPCAST_H
 
-@protocol SUAppcastDelegate;
+#if __has_feature(modules)
+@import Foundation;
+#else
+#import <Foundation/Foundation.h>
+#endif
+#import "SUExport.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @class SUAppcastItem;
-@interface SUAppcast : NSObject <NSURLDownloadDelegate>
+SU_EXPORT @interface SUAppcast : NSObject
 
-@property (weak) id<SUAppcastDelegate> delegate;
-@property (copy) NSString *userAgentString;
+@property (copy, nullable) NSString *userAgentString;
+@property (copy, nullable) NSDictionary<NSString *, NSString *> *httpHeaders;
 
-- (void)fetchAppcastFromURL:(NSURL *)url;
+- (void)fetchAppcastFromURL:(NSURL *)url inBackground:(BOOL)bg completionBlock:(void (^)(NSError *_Nullable))err;
+- (SUAppcast *)copyWithoutDeltaUpdates;
 
-@property (readonly, copy) NSArray *items;
+@property (readonly, copy, nullable) NSArray *items;
 @end
 
-@protocol SUAppcastDelegate <NSObject>
-- (void)appcastDidFinishLoading:(SUAppcast *)appcast;
-- (void)appcast:(SUAppcast *)appcast failedToLoadWithError:(NSError *)error;
-@end
+NS_ASSUME_NONNULL_END
 
 #endif
